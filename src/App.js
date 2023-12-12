7bcc929676a7753e145b98a582e2663f740d0497
Sanmi01 <supersanmi01@gmail.com>
2023-12-12 20:11:00 +0100

    Banner Section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import HeroSection1 from './components/HeroSection1';
+import BannerSection from './components/BannerSection';
 
 function App() {
   return (
     <div>
       <Header />
       <HeroSection1 />
+      <BannerSection />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -20,8 +20,22 @@ module.exports = {
           900: "#142a31",
           950: "#070e10",
         },
-        "brand-gray-light": "#F3F3F3"
+        "brand-gray-light": "#F3F3F3",
+        "brand-blue-dark": "#252E42",
       },
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      'md-custom2': '650px', // Custom breakpoint
+      'md-custom': '950px', // Custom breakpoint
+      'md-custom3': '680px', // Custom breakpoint
+      'max-md-custom3': {'max':'679px'}, // Custom breakpoint
+      'max-sm-custom': {'max':'450px'}, // Custom breakpoint
+      'max-sm-custom2': {'max':'495px'}, // Custom breakpoint
     },
   },
   plugins: [require("flowbite/plugin")],
