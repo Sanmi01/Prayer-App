a9d0f378f0ff12e5d5fb64abf31a0b893679fe01
Sanmi01 <supersanmi01@gmail.com>
2023-12-12 19:08:49 +0100

    HeroSection1 done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import HeroSection1 from './components/HeroSection1';
 
 function App() {
   return (
     <div>
       <Header />
+      <HeroSection1 />
     </div>
   );
 }
