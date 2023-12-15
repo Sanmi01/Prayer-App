427a6e8accc4a34475338ca2ac3d32d0f712488e
Sanmi01 <supersanmi01@gmail.com>
2023-12-15 20:41:43 +0100

    Disabled Resize

--- src/components/PrayerModal.jsx
+++ src/components/PrayerModal.jsx
@@ -54,7 +54,7 @@ const PrayerModal = ({ toggleModal, prayer, AddPrayer }) => {
                 value={prayerMessage}
                 id="message"
                 rows="4"
-                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="resize-none block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Share your request here and our community will pray for you."
                 maxLength="1000"
               ></textarea>
